48a5c6228bc61241b89fe3127203985eddc4f0c6
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-01-22 14:26:24 -0500

    Initial commit for the client-side application

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="title is-1 has-text-centered">Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
